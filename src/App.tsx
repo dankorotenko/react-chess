53cf08a2b230f0d633b2e827f999ebce3a381716
Daniel Korotenko <dankorotenko@Daniels-MacBook-Pro.local>
2022-05-16 23:18:26 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
+import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Player';
 const App = () => {
-  return (
-    <div>
+  const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
+  useEffect(() => {
+    restart()
+    setCurrentPlayer(whitePlayer)
+  }, [])
+  
 
+  function restart() {
+    const newBoard = new Board()
+    newBoard.initCells()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
+
+  function swapPlayer(){
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+  return (
+    <div className='app'>
+      <Timer 
+      restart={restart}
+      currentPlayer={currentPlayer}
+      />
+      <BoardComponent 
+      board={board}
+      setBoard={setBoard}
+      currentPlayer={currentPlayer}
+      swapPlayer={swapPlayer}
+      />
+      <div>
+        <LostFigures 
+        title="Black Figures"
+        figures={board.lostBlackFigures}
+        />
+        <LostFigures 
+        title="White Figures"
+        figures={board.lostWhiteFigures}
+        />
+      </div>
     </div>
   );
 };
